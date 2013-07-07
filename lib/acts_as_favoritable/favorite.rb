5011f46e8b883bad1235739701fb6a7a89be4f1d
Qi Li <cloudbsd@gmail.com>
2013-07-08 00:31:17 +0800

    use the rails 4 style syntax for migration file.

--- lib/acts_as_favoritable/favorite.rb
+++ lib/acts_as_favoritable/favorite.rb
@@ -3,13 +3,7 @@
     belongs_to :favoritable, :polymorphic => true
     belongs_to :favoriter, :polymorphic => true
 
-#   attr_accessible :favoritable_id, :favoritable_type,
-#                   :favoriter_id, :favoriter_type,
-#                   :favoritable, :favoriter
-
-    validates :favoritable_id, presence: true
-    validates :favoritable_type, presence: true
-    validates :favoriter_id, presence: true
-    validates :favoriter_type, presence: true
+    validates :favoritable, presence: true
+    validates :favoriter, presence: true
   end
 #end
--- lib/generators/acts_as_favoritable/migration/templates/active_record/migration.rb
+++ lib/generators/acts_as_favoritable/migration/templates/active_record/migration.rb
@@ -1,12 +1,10 @@
 class ActsAsFavoritableMigration < ActiveRecord::Migration
   def change
     create_table :favorites, :force => true do |t|
-      t.references :favoritable, :polymorphic => true, :null => false
-      t.references :favoriter, :polymorphic => true, :null => false
+      t.references :favoritable, :polymorphic => true, :null => false, index: true
+      t.references :favoriter, :polymorphic => true, :null => false, index: true
 
       t.timestamps
     end
-    add_index :favorites, [:favoritable_id, :favoritable_type]
-    add_index :favorites, [:favoriter_id, :favoriter_type]
   end
 end
